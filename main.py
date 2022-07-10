57886576fea74ea5746c2452b6a936affc0946c2
Simrit <kaulsimrit@gmail.com>
2022-07-10 19:22:21 +0530

    Edited the download link

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ def sketch_image(photo):
     grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Invert Image
-    invert_img = cv2.bitwise_not(grey_img)
+    img = cv2.bitwise_not(grey_img)
     # invert_img=255-grey_img
 
     # Blur image
-    blur_img = cv2.GaussianBlur(invert_img, (33, 33), 0)
+    img = cv2.GaussianBlur(img, (33, 33), 0)
 
     # Invert Blurred Image
-    invblur_img = cv2.bitwise_not(blur_img)
+    invblur_img = cv2.bitwise_not(img)
     # invblur_img=255-blur_img
 
     # Sketch Image
-    sketch_img = cv2.divide(grey_img, invblur_img, scale=256.0)
+    img = cv2.divide(grey_img, invblur_img, scale=256.0)
 
     # Save Sketch
-    cv2.imwrite('./static/images/sketch.png', sketch_img)
+    cv2.imwrite('./static/images/sketch.png', img)
 
     # Display sketch
     # cv2.imshow('sketch image', sketch_img)
